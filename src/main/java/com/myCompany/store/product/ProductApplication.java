18b41f63d400a77c8fa4e7aec7108af9000a0441
Divyaprakash M J <divyaprakashmj20@gmail.com>
2022-02-09 21:20:26 +0000

    hook test

--- src/main/java/com/myCompany/store/product/ProductApplication.java
+++ src/main/java/com/myCompany/store/product/ProductApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test hook");
 		SpringApplication.run(ProductApplication.class, args);
 	}
 
